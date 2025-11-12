9da93672624bb004452e34e9e65f8776573abfcc
Benjamin Maisonneuve <bmaisonneuve@gencovery.com>
2025-11-12 11:47:35 +0100

    (feat) remove downgrade when installing docker

--- utils/prepare_server.sh
+++ utils/prepare_server.sh
@@ -8,7 +8,7 @@ sudo apt-get -y update
 sudo apt-get -y install curl ca-certificates gnupg
 
 # Install docker and docker-compose (version 28)
-# Limit to version 28 due to imcopatibility of version 29 with Traefij : https://github.com/traefik/traefik/issues/12253
+# Limit to version 28 due to imcopatibility of version 29 with Traefik : https://github.com/traefik/traefik/issues/12253
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -22,7 +22,7 @@ sudo apt-get -y update
 # Ensure docker group has GID 999 before Docker installation
 sudo bash "${SCRIPT_DIR}/ensure_docker_gid.sh"
 
-sudo apt-get -y install --allow-downgrades docker-ce=5:28.* docker-ce-cli=5:28.* containerd.io
+sudo apt-get -y install docker-ce=5:28.* docker-ce-cli=5:28.* containerd.io
 sudo apt-get -y install docker-compose
 
 sudo usermod -aG docker $USER
